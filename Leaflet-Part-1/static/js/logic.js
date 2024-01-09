d88b2f903fd065591c5eabc7a7262503f75621ff
Mjkey2 <Mjmarentes2005@gmail.com>
2024-01-08 16:47:28 -0800

    ummm?

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,21 +1,36 @@
-// Create the map
-var map = L.map('map').setView([0, 0], 2);
-// Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18
-}).addTo(map);
+//Create map object
+var myMap = L.map("map", {
+  center: [40, -100],
+  zoom: 4
+});
+
+//Add tile layer
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(myMap);
+
+// Only one base layer can be shown at a time.
+let baseMaps = {
+  Street: street,
+  Topography: topo
+};
+
 // Create the key table
 var keyTable = '<table>';
 keyTable += '<tr><th>Depth</th><th>Color</th></tr>';
 keyTable += '<tr><td>0 - 10 km</td><td style="background-color: ' + getColor(10) + '"></td></tr>';
 keyTable += '<tr><td>10 - 20 km</td><td style="background-color: ' + getColor(20) + '"></td></tr>';
+keyTable += '<tr><td>20 - 30 km</td><td style="background-color: ' + getColor(30) + '"></td></tr>';
 // Add more rows for other depth ranges as needed
 keyTable += '</table>';
 // Add the key table to the HTML
 document.getElementById('key').innerHTML = keyTable;
+
 // Fetch the GeoJSON data
-fetch('static/js/all_week.geojson')
+earthquakes = fetch('static/js/all_week.geojson')
   .then(response => response.json())
   .then(data => {
     // Create a GeoJSON layer and add it to the map
@@ -37,45 +52,18 @@ fetch('static/js/all_week.geojson')
           fillOpacity: 0.8
         });
       }
-    }).addTo(map);
+    }).addTo(myMap);
   });
-// Function to determine the marker color based on earthquake depth
-function getColor(depth) {
-  // Define a color scale based on depth range
-  var colorScale = chroma.scale(['#ffffcc', '#800026']).domain([0, 100]);
-  // Map the depth value to a color using the color scale
-  var color = colorScale(depth).hex();
-  return color;
-}
-//d3.json("tectonicplates-master/GeoJSON/PB2002_plates.json").then(function(data) {
-//  var tectonicPlatesData = L.geoJSON(data);
-//  tectonicPlatesData.addTo(map);
-//});
-// Create a new layer group for the tectonic plates
-var tectonicPlates = new L.LayerGroup();
-// Fetch the tectonic plates data
-d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json", function(platesData) {
-  // Create a GeoJSON layer for the tectonic plates data
-  L.geoJSON(platesData, {
-    style: function(feature) {
-      return {
-        color: "orange",
-        weight: 2
-      };
-    }
-  }).addTo(tectonicPlates);
-});
-// Add the tectonic plates layer to the map
-tectonicPlates.addTo(map);
-// Create a baseMaps object to hold the base layers
-var baseMaps = {
-  "Street Map": streetMap,
-  "Topographic Map": topoMap
-};
-// Create an overlayMaps object to hold the overlay layers
-var overlayMaps = {
-  "Earthquakes": earthquakes,
-  "Tectonic Plates": tectonicPlates
-};
-// Add a layer control to the map
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+  // Function to determine the marker color based on earthquake depth
+  function getColor(depth) {
+    // Define a color scale based on depth range
+    var colorScale = chroma.scale(['#ffffcc', '#800026']).domain([0, 100]);
+    // Map the depth value to a color using the color scale
+    var color = colorScale(depth).hex();
+    return color;
+  }
+
+
+// Pass our map layers into our layer control.
+// Add the layer control to the map.
+L.control.layers(baseMaps).addTo(myMap);
